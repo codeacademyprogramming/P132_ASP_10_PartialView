82435de6a98d71b68edb172c7e32560fc1efa45d
eaghayev <eaghayevv@gmail.com>
2022-12-05 12:58:39 +0400

    Genre CRUD

---
+++ P132Pustok/Areas/Manage/Controllers/GenreController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P132Pustok.DAL;
+using P132Pustok.Models;
+
+namespace P132Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class GenreController : Controller
+    {
+        private readonly PustokContext _context;
+
+        public GenreController(PustokContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int page=1)
+        {
+            var model = _context.Genres.Include(x=>x.Books).Skip((page-1)*2).Take(2).ToList();
+            ViewBag.Page = page;
+            ViewBag.TotalPage = (int)Math.Ceiling(_context.Genres.Count() / 2d);
+
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Genre genre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if(_context.Genres.Any(x=>x.Name == genre.Name))
+            {
+                ModelState.AddModelError("Name", "This name has been taken");
+                return View();
+            }
+
+            _context.Genres.Add(genre);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Genre genre = _context.Genres.FirstOrDefault(x => x.Id == id);
+
+
+
+            return View(genre);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Edit(Genre genre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+
+            if(_context.Genres.Any(x=>x.Name == genre.Name && x.Id!=genre.Id))
+            {
+                ModelState.AddModelError("Name", "This name has been taken");
+                return View();
+            }
+
+            Genre existGenre = _context.Genres.FirstOrDefault(x => x.Id == genre.Id);
+
+            existGenre.Name = genre.Name;
+            _context.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Genre genre = _context.Genres.Include(x => x.Books).FirstOrDefault(x => x.Id == id);
+
+            return View(genre);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Genre genre)
+        {
+            Genre existGenre = _context.Genres.FirstOrDefault(x => x.Id == genre.Id);
+
+            if (!_context.Books.Any(x => x.GenreId == genre.Id))
+            {
+                _context.Genres.Remove(existGenre);
+                _context.SaveChanges();
+            }
+           
+
+            return RedirectToAction("index");
+        }
+
+    }
+}
--- P132Pustok/Models/Genre.cs
+++ P132Pustok/Models/Genre.cs
@@ -5,7 +5,10 @@ namespace P132Pustok.Models
     public class Genre
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Mecburidir, qaqa!")]
         [MaxLength(50)]
         public string Name { get; set; }
+
+        public List<Book>? Books { get; set; }
     }
 }
